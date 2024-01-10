e667bd7b528e03c43c164056c5bf6d323da5d9df
Paul Zwicker <pazwicker@gmail.com>
2024-01-09 22:25:58 -0600

    clumsy way to allow local testing v2

--- src/zwickfi/bigquery/bigquery.py
+++ src/zwickfi/bigquery/bigquery.py
@@ -5,13 +5,13 @@ from pathlib import Path
 
 class BigQuery(object):
     def write_to_bigquery(df, tablename):
-        try:
-            root = Path(__file__).parent.parent.parent.parent
-            relative_path = f'{root}/secrets/service-account-credentials.json'
-            absolute_path = os.path.abspath(relative_path)
-            os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = absolute_path
-        except:
-            pass
+        # try:
+        #     root = Path(__file__).parent.parent.parent.parent
+        #     relative_path = f'{root}/secrets/service-account-credentials.json'
+        #     absolute_path = os.path.abspath(relative_path)
+        #     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = absolute_path
+        # except:
+        #     pass
         """Load dataframe data into a specified BigQuery table."""
         table_id = f"zwickfi.monarch_money.{tablename}"
         client = bigquery.Client()
